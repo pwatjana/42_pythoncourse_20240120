6c8ef8d1759dc654d5da2615c17644dcbeb5e2df
pwatjana <ppwj10976@gmail.com>
2024-01-24 05:56:11 +0700

    Update 16-HW_RSP.py

--- 16-HW_RSP.py
+++ 16-HW_RSP.py
@@ -11,46 +11,53 @@
 # random pick items in lists and return first arg of the list.
 # return random value.
 def random():
-    #from rich import print
+    from rich import print
     import random
-    hand = random.choices(["rock", "scissor", "paper"])[0]
+    hand = random.choices(["rock", "scissor", "paper"])
     print(hand)
 random()
 
 # count round 
 def count(round: int):
-    count = 0
-    while round < 3:
-        print(round)
+     while round < 3:
+        print("Round ", round+1)
         round = round+1
-    print("End game : your score = ")
-
-
+        
 # determine winner and count win round
 def win(hand,user1):
     num=0 # initial score start 0
-    # winning conditions
+    while num ==2 :
+        break
+        print("Congrats! you win 2 out of 3 round ")
+    # winning conditions.
     if hand == 'rock' and user1 == 'p':
         num +=1
     elif hand == 'paper' and user1 == 's':
         num +=1
     elif hand == 'scissor' and user1 == 'r':
         num +=1  
-    print(num)
-    return num
+    print("End game : your score = ", num)
 
 
-def user1():
+def user1(type):
     my_list = ['r','s','p']
     if user1 not in my_list:
         print("You lose! Invalid Input. Please type r/p/s for rock/paper/scissor")
+    else:
+        return 
         
 
 
 
 def main():
-    user1 = input("Choose only one> r, s, p: ")
-    print("Computer choose : ", random)
+    hand = random    
+    user_choose = input("Enter your choice (r/p/s for rock/paper/) : ")
+    user1(user_choose)
+    win(hand,user1)
+
+        
+        
+
 
 
 if __name__ == "__main__":
